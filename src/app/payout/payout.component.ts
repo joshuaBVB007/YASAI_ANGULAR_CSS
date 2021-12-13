1eeae8176f5adcce467188b06562aa3bf17f0da1
Jonathan Calderon <jaksvafa7@gmail.com>
2021-12-13 20:39:16 +0100

    Total is finished tomorrow

--- src/app/payout/payout.component.ts
+++ src/app/payout/payout.component.ts
@@ -2,8 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { getAuth } from 'firebase/auth';
 import { getDatabase, ref, set } from 'firebase/database';
+import { __values } from 'tslib';
 import { app} from '../log-in/log-in.component';
 import { RestService } from '../rest.service';
+
+class ProductoAPagar{
+    nombre="";
+    precio="";
+    cantidad=0;
+    url="";
+    constructor(nombre:string,precio:string,cantidad:number,url:string){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.cantidad=cantidad;
+        this.url=url;
+    }
+}
 
 
 @Component({
@@ -14,18 +28,50 @@ import { RestService } from '../rest.service';
 
 export class PayoutComponent implements OnInit {
 
-
+  //usuario de firebase en activo
   userActive:any=getAuth().currentUser;
+
+  //la lista que recibimos de la observable
   lista_op:any[]=[];
+  //la lista que llenamos con los nuevos objetos
+  lista_A_pagar:any[]=[];
+  //Cantidad absoluta para pagar
   Total_A_Pagar:number=0;
   conversion:any;
+
 
   constructor(private service:RestService) { }
 
   ngOnInit(): void {
+    
     this.service.que_es="payout";
     this.lista_op=this.service.productos_en_el_carrito;
+    this.FillObject();
     console.log(this.lista_op);
+  }
+
+  SetQuantityOfProducts(indice:number,nombre:string,cantidad:string){
+    console.log(indice,nombre,cantidad);
+    this.lista_A_pagar[indice].cantidad=cantidad;
+  }
+
+  Confirm(){
+    for (let index = 0; index < this.lista_A_pagar.length; index++) {
+      const element = this.lista_A_pagar[index];
+      var process_price=parseFloat(element.precio)
+      var process_quantity=parseFloat(element.cantidad);
+      var process_total=process_price*process_quantity;
+      this.Total_A_Pagar+=process_total;
+    }
+  }
+
+  FillObject(){
+    for (let index = 0; index < this.lista_op.length; index++) {
+      const nombre = this.lista_op[index].nombre;
+      const price = this.lista_op[index].precio;
+      const url = this.lista_op[index].url;
+      this.lista_A_pagar.push(new ProductoAPagar(nombre,price,0,url));
+    }
   }
 
   DeleteProduct(indice:number){
