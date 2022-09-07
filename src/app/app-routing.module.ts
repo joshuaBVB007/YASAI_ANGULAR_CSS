b7c5cca104df22a42e3db632f92e2e315571ff7b
Jonathan Calderon <jaksvafa7@gmail.com>
2022-09-07 16:09:24 +0200

    we have eliminated some unnecessary files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ProductosComponent } from './productos/productos.component';
 import { ContactComponent } from './contact/contact.component';
-import { InfoProductComponent } from './info-product/info-product.component';
 import { LocationComponent } from './location/location.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { PayoutComponent } from './payout/payout.component';
 import { MiListaComponent } from './mi-lista/mi-lista.component';
-import { StageAreaComponent } from './stage-area/stage-area.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { HistorialComponent } from './historial/historial.component';
-import { OffersComponent } from './offers/offers.component';
 
 
 //Array de rutas validado
@@ -20,8 +17,6 @@ const routes: Routes = [
   {path:"products",component:ProductosComponent},
   {path:"contact",component:ContactComponent},
   {path:"location",component:LocationComponent},
-  {path:"stage",component:StageAreaComponent},
-  {path:"offers",component:OffersComponent},
   {path:"historial",component:HistorialComponent},
   {path:"login",component:LogInComponent},
   {path:"payout/:lista",component:PayoutComponent,
@@ -31,11 +26,7 @@ const routes: Routes = [
       component: MiListaComponent, // child route component that the router renders
     },
   ]},
-  {path:"details/:nombre",component:ProductDetailComponent,
-  children:[
-    {path:"info",component:InfoProductComponent},
-  ]
-  },
+  {path:"details/:nombre",component:ProductDetailComponent},
   {path:'', redirectTo: '/products',pathMatch:'full'},
   {path:"**",component:AboutUsComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,16 @@ import { ContactComponent } from './contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { PaginationComponent } from './pagination/pagination.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { MiListaComponent } from './mi-lista/mi-lista.component';
 import { CounterComponent } from './counter/counter.component';
-import { InfoProductComponent } from './info-product/info-product.component';
 import { SearchPipe } from './search.pipe';
 import { PayoutComponent } from './payout/payout.component';
-import { StageAreaComponent } from './stage-area/stage-area.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { HistorialComponent } from './historial/historial.component';
-import { OffersComponent } from './offers/offers.component';
 
 
 
@@ -35,18 +31,14 @@ import { OffersComponent } from './offers/offers.component';
     LocationComponent,
     ContactComponent,
     NavBarComponent,
-    PaginationComponent,
     ToolBarComponent,
     ProductDetailComponent,
     MiListaComponent,
-    InfoProductComponent,
     CounterComponent,
     SearchPipe,
     PayoutComponent,
-    StageAreaComponent,
     LogInComponent,
     HistorialComponent,
-    OffersComponent,
   ],
   imports: [
     BrowserModule,
